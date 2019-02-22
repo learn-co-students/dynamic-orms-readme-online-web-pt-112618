cab4de815376c33888514dad9737e6c435bd20f9
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-22 23:00:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-require_relative "../config/environment.rb"
+ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class Song
@@ -59,6 +59,3 @@ class Song
   end
 
 end
-
-
-
